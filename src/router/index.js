90b31a0700f17975b0459c954edec394ab1e4abe
aaaakkkkiiii <theaki14@gmail.com>
2020-11-10 15:17:18 +0900

    ちょっと試し

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/users/signup',
+    name: 'Home',
+    component: () => import('../views/UserSignup.vue')
   }
 ]
 
