6747be5dbfec360d2cdeb0a3a1f1be9fc773010a
tor <tor.one10101@gmail.com>
2020-12-08 17:47:06 +0900

    教員管理ページ　ユーザリスト追加（ボタンにアクションがない）

--- src/views/TeacherManager.vue
+++ src/views/TeacherManager.vue
@@ -20,15 +20,35 @@
           <th class="text-center">
             Name
           </th>
+          <th class="text-center">
+            Action
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr
-          v-for="user in users.users"
+          v-for="user in joinUsers.users"
           :key="user.id"
         >
           <td class="text-center">{{ user.user_id }}</td>
+          <td class="text-center">{{ user.user_name }}</td>
+          <td class="text-center">
+            <v-btn small color="primary" v-on:click="submit">
+              Delete
+            </v-btn>
+          </td>
+        </tr>
+        <tr
+          v-for="user in others"
+          :key="user.user_id"
+        >
+          <td class="text-center">{{ user.user_id }}</td>
           <td class="text-center">{{ user.name }}</td>
+          <td class="text-center">
+            <v-btn small color="error" v-on:click="submit">
+              Add
+            </v-btn>
+          </td>
         </tr>
       </tbody>
     </template>
@@ -43,20 +63,29 @@ export default {
   data: () => ({
     classId: 1,
     className: '',
-    users: []
+    joinUsers: [],
+    others: []
   }),
   mounted: async function () {
-    axios.get('http://aray.hibiki.work/users').then((response) => (this.users = response.data))
-    console.log(this.users)
+    this.joinUsers = (await axios.get('http://aray.hibiki.work/classes/', { params: { class_id: this.classId } })).data
+    const tmp = (await axios.get('http://aray.hibiki.work/users')).data
+    var tmptmp = []
+    for (let i = 0; i < tmp.users.length; ++i) {
+      let exists = false
+      for (let j = 0; j < this.joinUsers.users.length; ++j) {
+        if (tmp.users[i].user_id === this.joinUsers.users[j].user_id) {
+          exists = true
+          break
+        }
+      }
+      if (exists === false) {
+        tmptmp.push(tmp.users[i])
+      }
+    }
+    this.others = tmptmp
   },
   methods: {
     submit () {
-      axios.post(`https://aray.hibiki.work/answers/${this.questionId}`, {
-        answer_str: this.source,
-        file_type: 0
-      }).then((res) => {
-        console.log(res.status)
-      })
     }
   }
 }
