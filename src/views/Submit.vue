a5312922ab93b3a8fa508d995198eaca719398a2
tor <tor.one10101@gmail.com>
2020-12-08 13:17:07 +0900

    提出用ページ　APIの紐付けはまだ

--- src/views/Submit.vue
+++ src/views/Submit.vue
@@ -1,11 +1,39 @@
 <template>
-  <v-container
-    class="px-0"
-    fluid
-  >
-    <v-checkbox
-      v-model="checkbox"
-      :label="`Checkbox 1: ${checkbox.toString()}`"
-    ></v-checkbox>
+  <v-container fluid>
+    <v-textarea
+      auto-grow
+      v-model="source"
+      name="source-input"
+      label="ソースコードの提出"
+      hint="Hint text"
+      :value="file"
+    >{{ file }}</v-textarea>
+  <v-file-input
+   v-model="file"
+   truncate-length="15"
+   @change="fileinput"
+  ></v-file-input>
   </v-container>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    file: null,
+    source: ''
+  }),
+  methods: {
+    fileinput () {
+      console.log(this.file)
+      if (!this.file) this.source = 'No File Data'
+      else {
+        const reader = new FileReader()
+        reader.readAsText(this.file)
+        reader.onload = () => {
+          this.source = reader.result
+        }
+      }
+    }
+  }
+}
+</script>
