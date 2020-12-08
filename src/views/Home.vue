414c3715bce24e83814b8ba244f75bb3ee1b945e
aaaakkkkiiii <theaki14@gmail.com>
2020-12-08 15:51:37 +0900

    -_-

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <router-link to="/users/signup">Signup</router-link>
+    <router-link to="/users/signup">学生登録</router-link>
+    <br>
+    <router-link to="/teacher/signup">教員登録</router-link>
     <br>
     <router-link to="/about">About</router-link>
     <br>
-    <router-link to="/problem/">Questions</router-link>
+    <router-link to="/problem/">問題</router-link>
+    <br>
 
   </div>
 </template>
