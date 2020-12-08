ddbd277f40e53e2959ac98d5f11275cccf3c7559
aaaakkkkiiii <theaki14@gmail.com>
2020-12-08 16:32:07 +0900

    no message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
+    <router-link to="/">Home</router-link>
+    <br>
+    <router-link to="/about">About</router-link>
+    <br>
+    <router-link to="/login">Login</router-link>
+    <br>
+    <router-link to="/logout">Logout</router-link>
+    <br>
     <router-link to="/users/signup">学生登録</router-link>
     <br>
     <router-link to="/teacher/signup">教員登録</router-link>
     <br>
-    <router-link to="/about">About</router-link>
-    <br>
     <router-link to="/problem/">問題</router-link>
-    <br>
 
   </div>
 </template>
